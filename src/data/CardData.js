8706fc8469c391fa0ea126bdc180b0ebcfbe6757
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2024-08-02 00:45:17 +0700

    init

---
+++ src/data/CardData.js
@@ -0,0 +1,58 @@
+export const cardData = [
+  {
+    imageSrc: "/images/ydnb.png",
+    imageAlt: "YDNB",
+    desc: "Created an app for viewing account information, checking transactions, and performing transfers. It also supports sending money, transferring to external banks, and topping up wallets.",
+    links: [
+      {
+        href: "https://play.google.com/store/apps/details?id=com.ydnb.app001",
+        iconSrc: "/icons/playstore.svg",
+        iconAlt: "Play Store",
+      },
+      {
+        href: "https://apps.apple.com/us/app/ydnb-mbanking/id6449997906",
+        iconSrc: "/icons/appstore.svg",
+        iconAlt: "App Store",
+      },
+    ],
+  },
+  {
+    imageSrc: "/images/tcb.png",
+    imageAlt: "TCB",
+    desc: "Created an app for viewing account information, checking transactions, performing transfers, sending money, transferring to external banks, and facilitating mobile top-ups and electricity bill payments.",
+    links: [
+      {
+        href: "https://play.google.com/store/apps/details?id=com.tcb.app001",
+        iconSrc: "/icons/playstore.svg",
+        iconAlt: "Play Store",
+      },
+      {
+        href: "https://apps.apple.com/us/app/tcb-mbanking/id6450492816",
+        iconSrc: "/icons/appstore.svg",
+        iconAlt: "App Store",
+      },
+      {
+        href: "https://appgallery.cloud.huawei.com/ag/n/app/C109244609?locale=en_GB&source=appshare&subsource=C109244609&shareTo=com.viber.voip&shareFrom=appmarket&shareIds=855ee1bef8154ae6889dac6b2b766a7f_com.viber.voip&callType=SHARE",
+        iconSrc: "/icons/appgallery.svg",
+        iconAlt: "App Gallery",
+      },
+    ],
+  },
+  {
+    imageSrc: "/images/watchvault.png",
+    imageAlt: "Watch Vault",
+    desc: "Oversaw the development of a watch ordering platform with a seamless journey from product selection to checkout. Integrated real-time tracking and provided access to order history.",
+    links: [
+      {
+        href: "https://play.google.com/store/apps/details?id=com.watchvault.app001",
+        iconSrc: "/icons/playstore.svg",
+        iconAlt: "Play Store",
+      },
+      {
+        href: "https://apps.apple.com/us/app/watch-vault-by-diggie/id6469529196",
+        iconSrc: "/icons/appstore.svg",
+        iconAlt: "App Store",
+      },
+    ],
+  },
+];
---
+++ src/data/SkillData.js
@@ -0,0 +1,45 @@
+export const skillData = [
+  {
+    header: "Frontend Frameworks and Libraries",
+    skills: [
+      { src: "/skills/angular.svg", alt: "Angular", tooltip: "Angular" },
+      { src: "/skills/react.svg", alt: "React", tooltip: "React" },
+      { src: "/skills/vue.svg", alt: "Vue.js", tooltip: "Vue.js" },
+      { src: "/skills/astro.svg", alt: "Astro", tooltip: "Astro" },
+      { src: "/skills/flutter.svg", alt: "Flutter", tooltip: "Flutter" },
+      { src: "/skills/ionic.svg", alt: "Ionic", tooltip: "Ionic" },
+      { src: "/skills/socketio.svg", alt: "Socket.IO", tooltip: "Socket.IO" },
+    ],
+  },
+  {
+    header: "Backend Frameworks",
+    skills: [
+      { src: "/skills/java.svg", alt: "Java", tooltip: "Java" },
+      { src: "/skills/nodejs.svg", alt: "Node.js", tooltip: "Node.js" },
+      { src: "/skills/python.svg", alt: "Python", tooltip: "Python" },
+      { src: "/skills/go.svg", alt: "Go", tooltip: "Go" },
+    ],
+  },
+  {
+    header: "Databases",
+    skills: [
+      { src: "/skills/mssql.svg", alt: "MSSQL", tooltip: "MSSQL" },
+      {
+        src: "/skills/postgresql.svg",
+        alt: "PostgreSQL",
+        tooltip: "PostgreSQL",
+      },
+      { src: "/skills/mongodb.svg", alt: "MongoDB", tooltip: "MongoDB" },
+    ],
+  },
+  {
+    header: "DevOps and Cloud",
+    skills: [
+      { src: "/skills/gitlab.svg", alt: "GitLab", tooltip: "GitLab" },
+      { src: "/skills/github.svg", alt: "GitHub", tooltip: "GitHub" },
+      { src: "/skills/docker.svg", alt: "Docker", tooltip: "Docker" },
+      { src: "/skills/firebase.svg", alt: "Firebase", tooltip: "Firebase" },
+      { src: "/skills/azure.svg", alt: "Azure", tooltip: "Azure" },
+    ],
+  },
+];
