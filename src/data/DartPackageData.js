5f057ae825bf400a1caf7dad0aa332f2dd81262f
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2024-08-30 14:48:30 +0700

    add packages

---
+++ src/data/DartPackageData.js
@@ -0,0 +1,12 @@
+export const dartPackageData = [
+  {
+    imageSrc: "/images/api_client_generator.png",
+    imageAlt: "API Client Generator",
+    href: "https://pub.dev/packages/api_client_generator",
+  },
+  {
+    imageSrc: "/images/git_emoji_enhancer.png",
+    imageAlt: "Git Emoji Enhancer",
+    href: "https://pub.dev/packages/git_emoji_enhancer",
+  },
+];
---
+++ src/data/NPMPackageData.js
@@ -0,0 +1,12 @@
+export const npmPackageData = [
+  {
+    imageSrc: "/images/mock-api-gateway.png",
+    imageAlt: "Mock API Gateway",
+    href: "https://www.npmjs.com/package/mock-api-gateway",
+  },
+  {
+    imageSrc: "/images/mock-schema-generator.png",
+    imageAlt: "Mock Schema Generator",
+    href: "https://www.npmjs.com/package/mock-schema-generator",
+  },
+];
