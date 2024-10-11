317a1eae9900523cd26b2cea04f2caf7e9d9b239
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2024-10-11 11:11:10 +0700

    modify certify

--- src/data/CertificateData.js
+++ src/data/CertificateData.js
@@ -1,19 +1,7 @@
 export const certificateData = [
   {
-    href: "https://www.hackerrank.com/certificates/154fb076f993",
-    title: "Problem Solving (Basic)",
-    iconSrc: "/icons/link.svg",
-    iconAlt: "Link",
-  },
-  {
     href: "https://www.hackerrank.com/certificates/db94260451da",
     title: "Problem Solving (Intermediate)",
-    iconSrc: "/icons/link.svg",
-    iconAlt: "Link",
-  },
-  {
-    href: "https://www.hackerrank.com/certificates/a7fa55181632",
-    title: "Software Engineer Intern",
     iconSrc: "/icons/link.svg",
     iconAlt: "Link",
   },
@@ -24,32 +12,14 @@ export const certificateData = [
     iconAlt: "Link",
   },
   {
-    href: "https://www.hackerrank.com/certificates/bd65e2b527bc",
-    title: "JavaScript (Basic)",
-    iconSrc: "/icons/link.svg",
-    iconAlt: "Link",
-  },
-  {
     href: "https://www.hackerrank.com/certificates/2f51d5277588",
     title: "JavaScript (Intermediate)",
     iconSrc: "/icons/link.svg",
     iconAlt: "Link",
   },
   {
-    href: "https://www.hackerrank.com/certificates/af2c24b74b40",
-    title: "Angular (Basic)",
-    iconSrc: "/icons/link.svg",
-    iconAlt: "Link",
-  },
-  {
     href: "https://www.hackerrank.com/certificates/5d92b1babe4e",
     title: "Angular (Intermediate)",
-    iconSrc: "/icons/link.svg",
-    iconAlt: "Link",
-  },
-  {
-    href: "https://www.hackerrank.com/certificates/0eed46c253ea",
-    title: "React (Basic)",
     iconSrc: "/icons/link.svg",
     iconAlt: "Link",
   },
@@ -84,26 +54,8 @@ export const certificateData = [
     iconAlt: "Link",
   },
   {
-    href: "https://www.hackerrank.com/certificates/ad5fc5da580b",
-    title: "Go (Basic)",
-    iconSrc: "/icons/link.svg",
-    iconAlt: "Link",
-  },
-  {
     href: "https://www.hackerrank.com/certificates/09035745afbe",
     title: "Go (Intermediate)",
-    iconSrc: "/icons/link.svg",
-    iconAlt: "Link",
-  },
-  {
-    href: "https://www.hackerrank.com/certificates/8a0f64a6243a",
-    title: "SQL (Basic)",
-    iconSrc: "/icons/link.svg",
-    iconAlt: "Link",
-  },
-  {
-    href: "https://www.hackerrank.com/certificates/934e4aed9009",
-    title: "SQL (Intermediate)",
     iconSrc: "/icons/link.svg",
     iconAlt: "Link",
   },
